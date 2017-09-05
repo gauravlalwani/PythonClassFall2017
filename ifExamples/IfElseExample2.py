65e39b2f6a515cf374e81e5b72c62d977f8de045
Satoru Hayasaka <hayasaka@utexas.edu>
2017-09-05 10:57:19 -0500

    If...else example 2

---
+++ ifExamples/IfElseExample2.py
@@ -0,0 +1,10 @@
+age = 44
+hand = 'left'
+if (age<20) & (hand=='right'):
+    print('The age is ' + str(age) + ' years old.')
+    print('The handedness is ' + hand + '-handed.')
+    print('Meeting all the requirements.')
+else:
+    print('Age requirement: younger than 20')
+    print('Handedness requirement: right-handed')
+    print('You did not meet either requirement')
