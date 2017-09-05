56a14628bded83ca736b6e6237aff60f5bda6154
Satoru Hayasaka <hayasaka@utexas.edu>
2017-09-05 10:52:52 -0500

    If example 2

---
+++ ifExamples/IfExample2.py
@@ -0,0 +1,8 @@
+age = 23
+hand = 'right'
+# evaluating the age and handedness requirements at once
+if (age>=18) and (hand=='right'):
+    print('The age is ' + str(age) + ' years old.')
+    print('The handedness is ' + hand + '-handed.')
+    print('Meeting all the requirements.')
+
