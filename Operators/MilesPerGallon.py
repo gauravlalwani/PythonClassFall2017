82849c07ebc5633acbdb93bf7694fb232e5f54fc
Satoru Hayasaka <hayasaka@utexas.edu>
2017-09-05 15:52:53 -0500

    Miles per gallon exercise

---
+++ Operators/MilesPerGallon.py
@@ -0,0 +1,7 @@
+# user inputs
+miles = float(input('How many miles have you driven? '))
+fuel = float(input('How much fuel did you use? '))
+# calculating mpg
+mpg = miles / fuel
+# printing the result
+print('MPG: ' + str(mpg))
