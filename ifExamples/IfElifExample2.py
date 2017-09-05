93af530688d71062a4bdf4b7073dd9fbde4a7370
Satoru Hayasaka <hayasaka@utexas.edu>
2017-09-05 11:41:15 -0500

    If elif example 2

---
+++ ifExamples/IfElifExample2.py
@@ -0,0 +1,10 @@
+age = 19
+hand = 'left'
+if (age<20) & (hand=='right'):
+    print('Meeting all the requirements.')
+elif age<20:
+    print('Age requirement is met')
+else:
+    print('None of the requirements is met')
+
+
