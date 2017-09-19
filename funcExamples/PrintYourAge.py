0b6a0435b28327fd7832b3148e8ebea2ee188c14
Satoru Hayasaka <hayasaka@utexas.edu>
2017-09-19 15:29:01 -0500

    Print your age exercise

---
+++ funcExamples/PrintYourAge.py
@@ -0,0 +1,7 @@
+def PrintYourAge(age):
+    print('You are ' + str(age) + ' years old.')
+    if age<=30:
+        print('You are eligible to participate.')
+    else:
+        print('You are not eligible to participate.')
+
