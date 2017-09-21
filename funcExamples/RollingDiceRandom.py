7bfa743e1c9eac6fcbd4c49c9e0e406543ca3038
Satoru Hayasaka <hayasaka@utexas.edu>
2017-09-21 15:37:56 -0500

    Rolling dice randomly exercise

---
+++ funcExamples/RollingDiceRandom.py
@@ -0,0 +1,11 @@
+import random
+
+while True:
+    a = random.randint(1,6)
+    b = random.randint(1,6)
+    print('(' + str(a) + ', ' + str(b) + ')')
+    if a==1 and b==1:
+        break
+
+
+        
