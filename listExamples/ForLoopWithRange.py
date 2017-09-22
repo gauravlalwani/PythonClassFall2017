67073b7edf30858a8f228593cee5adb122a5ca3a
Satoru Hayasaka <hayasaka@utexas.edu>
2017-09-22 16:19:36 -0500

    For loop with a range function

---
+++ listExamples/ForLoopWithRange.py
@@ -0,0 +1,4 @@
+day = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+menu = ['Chili', 'Hot Dog', 'Salad', 'Tacos', 'Pizza']
+for i in range(len(day)):
+    print(day[i] + "'s special is " + menu[i])
