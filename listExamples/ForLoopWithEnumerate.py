f42a6433791a0135fe4d2f41059defec3d93ddac
Satoru Hayasaka <hayasaka@utexas.edu>
2017-09-22 16:21:54 -0500

    For loop with enumerate

---
+++ listExamples/ForLoopWithEnumerate.py
@@ -0,0 +1,4 @@
+day = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+menu = ['Chili', 'Hot Dog', 'Salad', 'Tacos', 'Pizza']
+for i, iDay in enumerate(day):
+    print(iDay + "'s special is " + menu[i])
