ddece79d62a6fb2084a8bbe8238f4265e463b30f
Satoru Hayasaka <hayasaka@utexas.edu>
2017-10-10 11:31:49 -0500

    Formatting numbers

---
+++ fileExamples/PrintRandNum.py
@@ -0,0 +1,14 @@
+import random
+
+for i in range(3):
+    a = random.random() * 300
+    print('%8.3f' % a)
+
+for i in range(3):
+    a = random.random() * 300
+    print('%10.3f' % a)
+
+for i in range(3):
+    a = random.random() * 300
+    print('%12.3f' % a)
+
