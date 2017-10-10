d8379f31a11aa1a060116b7081792c39a7a49a9e
Satoru Hayasaka <hayasaka@utexas.edu>
2017-10-10 11:13:37 -0500

    Added the directory name

--- fileExamples/ReadWithReadlines.py
+++ fileExamples/ReadWithReadlines.py
@@ -1,5 +1,5 @@
 # first, open the file
-f = open('MultiLineData.txt', 'r')
+f = open('TestData/MultiLineData.txt', 'r')
 # read the file into a list called fileData.
 fileData = f.readlines()
 # always close the file you opened
