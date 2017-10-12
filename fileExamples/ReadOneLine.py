059f9df4928054f307af8081a7458e8cba86e4ea
Satoru Hayasaka <hayasaka@utexas.edu>
2017-10-12 15:08:48 -0500

    Converting the relative path with os.path.join()

--- fileExamples/ReadOneLine.py
+++ fileExamples/ReadOneLine.py
@@ -1,5 +1,7 @@
+import os
+
 # first, open the file
-f = open('TestData/SingleLineData.txt', 'r')
+f = open(os.path.join('TestData','SingleLineData.txt'), 'r')
 # read a line, then put in a variable called line
 line = f.readline()
 # always close the file you opened
