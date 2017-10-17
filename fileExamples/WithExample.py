010afa140d5eeb059053d71ca2d9ad0abee76f5e
Satoru Hayasaka <hayasaka@utexas.edu>
2017-10-17 09:38:20 -0500

    Now it uses os.path.join so that it runs on Windows machines

--- fileExamples/WithExample.py
+++ fileExamples/WithExample.py
@@ -1,13 +1,15 @@
+import os
+
 stateList = []
 capitalList = []
-with open('TestData/StateCapitalList.txt','r') as infile:
+with open(os.path.join('TestData','StateCapitalList.txt'),'r') as infile:
     for line in infile:
         state, capital = line.strip().split(',')
         stateList.append(state)
         capitalList.append(capital)
         print('State: ' + '%-15s' % state + '\tCapital: ' + capital)
 
-with open('TestData/FormatStateList.txt','w') as outfile:
+with open(os.path.join('TestData','FormatStateList.txt'),'w') as outfile:
     for i, state in enumerate(stateList):
         outfile.write('State: ' + '%-15s' % state + '\tCapital: ' + capitalList[i])
         outfile.write('\n')
