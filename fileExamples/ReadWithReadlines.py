b3865f33a33e381886bdb5039c451a999c110ffc
Satoru Hayasaka <hayasaka@utexas.edu>
2017-10-17 09:27:54 -0500

    Now it uses os.path.join so that it runs on Windows machines

--- fileExamples/ReadWithReadlines.py
+++ fileExamples/ReadWithReadlines.py
@@ -1,5 +1,7 @@
+import os
+
 # first, open the file
-f = open('TestData/MultiLineData.txt', 'r')
+f = open(os.path.join('TestData','MultiLineData.txt'), 'r')
 # read the file into a list called fileData.
 fileData = f.readlines()
 # always close the file you opened
