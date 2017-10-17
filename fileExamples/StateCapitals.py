2b06c186714e59f1f869ee65728c2b5979080355
Satoru Hayasaka <hayasaka@utexas.edu>
2017-10-17 15:36:49 -0500

    State capital list exercise

---
+++ fileExamples/StateCapitals.py
@@ -0,0 +1,17 @@
+import os
+
+# reading the data
+f = open(os.path.join('TestData','StateCapitalList.txt'), 'r')
+fileData = f.read()
+f.close()
+
+# split into lines
+lines = fileData.strip().split('\n')
+
+# putting them in lists
+state = []
+capital = []
+for iState in lines:
+    items = iState.split(',')
+    state.append(items[0])
+    capital.append(items[1])
