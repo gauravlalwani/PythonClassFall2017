7b15d3bd266a942e8541829a176ce321d59b606b
Satoru Hayasaka <hayasaka@utexas.edu>
2017-11-07 11:20:57 -0600

    Error bar exercise data

---
+++ plotExamples/ErrorBarExercise.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+meanData = np.array([41.2, 35.1, 29.5, 22.9, 43.4, 39.8, 36.6, 32.9])
+x = np.array([10,20,30,40,10,20,30,40])
+sdData = np.array([3.4, 3.8, 3.3, 3.9, 4.0, 3.2, 3.4, 3.5])
+cond = np.array([1,1,1,1,2,2,2,2])
+
+
+# plotting the data and error bars separately
+plt.plot(x[cond==1], meanData[cond==1], 'r-', label='Condition 1')
+plt.plot(x[cond==2], meanData[cond==2], 'b-', label='Condition 2')
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.axis([8,42,15,50])
+plt.legend()
+plt.show()
+
+             
