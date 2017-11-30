2956123582278cdfa2b76f0a62fc3964a270b7fa
Satoru Hayasaka <hayasaka@utexas.edu>
2017-11-30 10:29:51 -0600

    Label widget example

---
+++ tkinterExamples/Label.py
@@ -0,0 +1,11 @@
+from tkinter import Tk,Label
+
+# initializing the root window
+root = Tk()
+
+# displaying the label widget
+x = Label(root,text="Hello World")
+x.pack()
+
+# keep the window displayed
+root.mainloop()
